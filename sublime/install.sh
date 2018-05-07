f2a74877c26ca1b251634675aa4c3894bc9d7c20
Timothy Flynn <timothy.flynn@viasat.com>
2018-05-07 13:23:34 -0400

    Fix install.sh script

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,5 +1,5 @@
-SUBLIME_DIR=$HOME/Sublime
-HOST_OS=$(uname -s)
+SUBLIME_DIR="$HOME/Sublime"
+HOST_OS="$(uname -s)"
 
 clone_or_update()
 {
@@ -11,17 +11,15 @@ clone_or_update()
     local github="https://github.com/trflynn89"
     local project="$1"
 
-    if [[ -d $SUBLIME_DIR/$project ]] ; then
-        git -C $SUBLIME_DIR/$project pull
+    if [[ -d "$SUBLIME_DIR/$project" ]] ; then
+        git -C "$SUBLIME_DIR/$project" pull
 
         if [[ $? -ne 0 ]] ; then
             echo "Could not update $project.git"
             exit 1
         fi
-
-        popd
     else
-        git clone $github/$project.git $SUBLIME_DIR/$project
+        git clone $github/$project.git "$SUBLIME_DIR/$project"
 
         if [[ $? -ne 0 ]] ; then
             echo "Could not clone $project.git"
@@ -31,9 +29,9 @@ clone_or_update()
 }
 
 if [[ $HOST_OS == "Darwin" ]] ; then
-    dst_dir="~/Library/Application Support/Sublime Text 3/Packages"
+    dst_dir="$HOME/Library/Application Support/Sublime Text 3/Packages"
 elif [[ $HOST_OS == "Linux" ]] ; then
-    dst_dir="~/.config/sublime-text-3/Packages"
+    dst_dir="$HOME/.config/sublime-text-3/Packages"
 else
     echo "Unknown host: $(HOST_OS)"
     exit 1
@@ -43,13 +41,13 @@ clone_or_update dotfiles
 clone_or_update Packages
 clone_or_update Seti_UI
 
-ln -sf $SUBLIME_DIR/dotfiles/sublime/Preferences.sublime-settings $dst_dir/User
+ln -sf "$SUBLIME_DIR/dotfiles/sublime/Preferences.sublime-settings" "$dst_dir/User"
 
-ln -sf $SUBLIME_DIR/dotfiles/sublime/Flynn $dst_dir
-ln -sf $SUBLIME_DIR/dotfiles/sublime/MIB $dst_dir
-ln -sf $SUBLIME_DIR/dotfiles/sublime/YANG $dst_dir
+ln -sf "$SUBLIME_DIR/dotfiles/sublime/Flynn" "$dst_dir"
+ln -sf "$SUBLIME_DIR/dotfiles/sublime/MIB" "$dst_dir"
+ln -sf "$SUBLIME_DIR/dotfiles/sublime/YANG" "$dst_dir"
 
-ln -sf $SUBLIME_DIR/Packages/C++ $dst_dir
-ln -sf $SUBLIME_DIR/Packages/Makefile2 $dst_dir/Makefile
+ln -sf "$SUBLIME_DIR/Packages/C++" "$dst_dir"
+ln -sf "$SUBLIME_DIR/Packages/Makefile2" "$dst_dir/Makefile"
 
-ln -sf $SUBLIME_DIR/Seti_UI $dst_dir/Seti_UI
+ln -sf "$SUBLIME_DIR/Seti_UI" "$dst_dir"
