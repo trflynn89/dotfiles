2c22babf6b34d161840cef15250b512f54659bfc
Timothy Flynn <timothy.flynn@viasat.com>
2018-09-18 15:39:48 -0400

    The view's window() sometimes returns None during __init__

--- sublime/Flynn/copy_path_from_project_folder.py
+++ sublime/Flynn/copy_path_from_project_folder.py
@@ -6,7 +6,13 @@ import sublime_plugin
 class CopyPathFromProjectFolderCommand(sublime_plugin.TextCommand):
     def __init__(self, *args, **kwargs):
         super(CopyPathFromProjectFolderCommand, self).__init__(*args, **kwargs)
+        self.relative_file_path = None
 
+    def run(self, edit):
+        sublime.set_clipboard(self.relative_file_path)
+        sublime.status_message("Copied file path")
+
+    def is_enabled(self):
         window_variables = self.view.window().extract_variables()
 
         project_path = window_variables['folder']
@@ -14,12 +20,5 @@ class CopyPathFromProjectFolderCommand(sublime_plugin.TextCommand):
 
         if project_path and file_path and file_path.startswith(project_path):
             self.relative_file_path = os.path.relpath(file_path, project_path)
-        else:
-            self.relative_file_path = None
 
-    def run(self, edit):
-        sublime.set_clipboard(self.relative_file_path)
-        sublime.status_message("Copied file path")
-
-    def is_enabled(self):
         return bool(self.relative_file_path)
