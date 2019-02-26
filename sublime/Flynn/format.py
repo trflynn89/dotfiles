2e6f2fa05f53a12f7e8669816a7cfa4675d82e35
Timothy Flynn <trflynn89@pm.me>
2019-02-25 21:26:07 -0500

    Cleanup and refactor format plugin Move large chunks of code into smaller functions.
    Instead of using a listener, always re-find the clang-format binary. I'd rather
    not do this, but even with the listener, a view would not see configuration
    changes until that view was closed/re-opened. So this at least prevents that.
    
    Exhaustively search any configured clang_format_directory before checking PATH.

--- sublime/Flynn/format.py
+++ sublime/Flynn/format.py
@@ -5,25 +5,88 @@ import subprocess
 import sublime
 import sublime_plugin
 
+# List of possible names the clang-format binary may have
 FORMATTERS = ['clang-format']
 if os.name == 'nt':
     FORMATTERS.extend(['clang-format.bat', 'clang-format.exe'])
 
-LANGUAGES = ('C', 'C++', 'Objective-C', 'Objective-C++', 'Java')
-ENCODING = 'utf-8'
+# List of languages supported for use with clang-format
+LANGUAGES = ('C', 'C++', 'Objective-C', 'Objective-C++', 'Java', 'JavaScript')
 
-def get_project_setting(window, setting_key):
+def get_project_setting(setting_key):
     """
-    Load a project setting from the given window, with environment variable
+    Load a project setting from the active window, with environment variable
     expansion.
     """
-    project_data = window.project_data()
+    project_data = sublime.active_window().project_data()
 
     if project_data and ('settings' in project_data):
         settings = project_data['settings']
 
         if setting_key in settings:
             return os.path.expandvars(settings[setting_key])
+
+    return None
+
+def find_binary(directory, binaries):
+    """
+    Search for one of a list of binaries in the given directory or on the system
+    PATH. Return the first valid binary that is found.
+    """
+    is_directory = lambda d: d and os.path.isdir(d) and os.access(d, os.R_OK)
+    is_binary = lambda f: f and os.path.isfile(f) and os.access(f, os.X_OK)
+
+    # First search through the given directory for any of the binaries
+    for binary in (binaries if is_directory(directory) else []):
+        binary = os.path.join(directory, binary)
+        if is_binary(binary):
+            return binary
+
+    # Then fallback onto the system PATH
+    for binary in binaries:
+        binary = shutil.which(binary)
+        if is_binary(binary):
+            return binary
+
+    return None
+
+def execute_command(command, working_directory, stdin=None):
+    """
+    Execute a command list in the given working directory, optionally piping in
+    an input string. Returns the standard output of the command, or None if an
+    error occurred.
+    """
+    startup_info = None
+
+    # On Windows, prevent a command prompt from showing
+    if os.name == 'nt':
+        startup_info = subprocess.STARTUPINFO()
+        startup_info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+    try:
+        encoding = 'utf-8'
+
+        process = subprocess.Popen(
+            command,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=working_directory,
+            startupinfo=startup_info,
+        )
+
+        if stdin:
+            stdin = stdin.encode(encoding)
+
+        (stdout, stderr) = process.communicate(input=stdin)
+
+        if stderr:
+            sublime.error_message('Error: ' + stderr.decode(encoding))
+        elif stdout:
+            return stdout.decode(encoding)
+
+    except Exception as ex:
+        sublime.error_message('Exception: ' + str(ex))
 
     return None
 
@@ -35,78 +98,34 @@ class FormatFileCommand(sublime_plugin.TextCommand):
     This plugin by default loads clang-format from the system PATH. But because
     Sublime doesn't source ~/.zshrc or ~/.bashrc, any PATH changes made there
     will not be noticed. So, users may set "clang_format_directory" in their
-    project's settings, and that directory is used instead of PATH. Any known
-    environment variables in the setting's value will be expanded. For example:
+    project's settings, and that directory is used instead of PATH. Example:
 
         {
             "folders": [],
             "settings": {
-                "clang_format_directory": "$HOME/workspace/depot_tools",
+                "clang_format_directory": "$HOME/workspace/tools",
             }
         }
+
+    Any known environment variables in the setting's value will be expanded.
     """
-    def __init__(self, *args, **kwargs):
-        super(FormatFileCommand, self).__init__(*args, **kwargs)
-
-        formatter_directory = get_project_setting(
-            sublime.active_window(),
-            'clang_format_directory'
-        )
-
-        valid = lambda f: f and os.path.isfile(f) and os.access(f, os.X_OK)
-
-        for formatter in FORMATTERS:
-            if formatter_directory:
-                candidate = os.path.join(formatter_directory, formatter)
-            else:
-                candidate = shutil.which(formatter)
-
-            if valid(candidate):
-                self.formatter = candidate
-                break
-
-        else:
-            self.formatter = None
-
     def run(self, edit):
         command = [self.formatter, '-assume-filename', self.view.file_name()]
 
         for region in self.view.sel():
-            if region.empty():
-                continue
+            if not region.empty():
+                command.extend(['-offset', str(region.begin())])
+                command.extend(['-length', str(region.size())])
 
-            command.extend(['-offset', str(region.begin())])
-            command.extend(['-length', str(region.size())])
+        working_directory = os.path.dirname(self.view.file_name())
 
         region = sublime.Region(0, self.view.size())
         contents = self.view.substr(region)
 
-        try:
-            startupinfo = None
-            if os.name == 'nt':
-                startupinfo = subprocess.STARTUPINFO()
-                startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        contents = execute_command(command, working_directory, stdin=contents)
 
-            process = subprocess.Popen(
-                command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                stdin=subprocess.PIPE,
-                startupinfo=startupinfo,
-                cwd=os.path.dirname(self.view.file_name())
-            )
-
-            (formatted, error) = process.communicate(contents.encode(ENCODING))
-        except Exception as ex:
-            sublime.error_message('Format exception: ' + str(ex))
-            (formatted, error) = (None, None)
-
-        if error:
-            sublime.error_message('Format error: ' + error.decode(ENCODING))
-
-        elif formatted:
-            sublime.status_message('Formatted file')
-            self.view.replace(edit, region, formatted.decode(ENCODING))
+        if contents:
+            self.view.replace(edit, region, contents)
 
     def is_enabled(self):
         (syntax, _) = os.path.splitext(self.view.settings().get('syntax'))
@@ -115,15 +134,7 @@ class FormatFileCommand(sublime_plugin.TextCommand):
         return supported and bool(self.view.file_name())
 
     def is_visible(self):
+        clang_format_directory = get_project_setting('clang_format_directory')
+        self.formatter = find_binary(clang_format_directory, FORMATTERS)
+
         return bool(self.formatter)
-
-class FormatFileListener(sublime_plugin.EventListener):
-    """
-    Listener to reload the format file plugin when the project settings have
-    changed.
-    """
-    def on_post_save_async(self, view):
-        window = view.window()
-
-        if view.file_name() == window.project_file_name():
-            sublime_plugin.reload_plugin(__name__)
