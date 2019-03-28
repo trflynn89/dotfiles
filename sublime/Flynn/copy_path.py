b37723c79d875641a40b6b685398deeb6f3c93aa
Timothy Flynn <timothy.flynn@viasat.com>
2019-03-28 15:05:00 -0400

    Convert C files to header files where desired

--- sublime/Flynn/copy_path.py
+++ sublime/Flynn/copy_path.py
@@ -10,6 +10,7 @@ class RelativePath(object):
     project's root directory.
     """
     def __init__(self):
+        self.project_path = None
         self.relative_path = None
 
     def __call__(self, view):
@@ -23,10 +24,47 @@ class RelativePath(object):
             candidates = [p for p in project_paths if file_path.startswith(p)]
 
             if candidates:
-                project_path = min(candidates, key=len)
-                self.relative_path = os.path.relpath(file_path, project_path)
+                self.project_path = min(candidates, key=len)
+                self.relative_path = os.path.relpath(file_path, self.project_path)
 
-        return self.relative_path
+        return (self.relative_path, self.project_path)
+
+class RelativePathCommand(sublime_plugin.TextCommand):
+    """
+    Base class for commands which need relative path information.
+    """
+    def __init__(self, *args, **kwargs):
+        super(RelativePathCommand, self).__init__(*args, **kwargs)
+        self.relative_path = RelativePath()
+
+    def is_enabled(self):
+        return all(self.relative_path(self.view))
+
+class CFamilyCommand(RelativePathCommand):
+    """
+    Base class for commands specific to the C/C++ family of languages.
+    """
+    LANGUAGES = ('C', 'C++', 'Objective-C', 'Objective-C++')
+    HEADERS = ('.h', '.hh', '.hpp')
+
+    def to_header_file(self):
+        (relative_path, project_path) = self.relative_path(self.view)
+        (path, ext) = os.path.splitext(relative_path)
+
+        if ext not in self.HEADERS:
+            for header in self.HEADERS:
+                new_path = path + header
+
+                if os.path.isfile(os.path.join(project_path, new_path)):
+                    return new_path
+
+        return relative_path
+
+    def is_enabled(self):
+        (syntax, _) = os.path.splitext(self.view.settings().get('syntax'))
+        supported = any(syntax.endswith(lang) for lang in self.LANGUAGES)
+
+        return supported and super(CFamilyCommand, self).is_enabled()
 
 class CopyFileNameCommand(sublime_plugin.TextCommand):
     """
@@ -50,80 +88,48 @@ class CopyFileDirectoryCommand(sublime_plugin.TextCommand):
     def is_enabled(self):
         return bool(self.view.file_name())
 
-class CopyFilePathRelativeToProjectCommand(sublime_plugin.TextCommand):
+class CopyFilePathRelativeToProjectCommand(RelativePathCommand):
     """
     Command to copy the path of the current file relative to its project's root
     directory.
     """
-    def __init__(self, *args, **kwargs):
-        super(CopyFilePathRelativeToProjectCommand, self).__init__(
-            *args, **kwargs)
-        self.relative_path = RelativePath()
-
     def run(self, edit):
-        sublime.set_clipboard(self.relative_path(self.view))
+        (relative_path, _) = self.relative_path(self.view)
+
+        sublime.set_clipboard(relative_path)
         sublime.status_message('Copied relative file')
 
-    def is_enabled(self):
-        return bool(self.relative_path(self.view))
-
-class CopyFileDirectoryRelativeToProjectCommand(sublime_plugin.TextCommand):
+class CopyFileDirectoryRelativeToProjectCommand(RelativePathCommand):
     """
     Command to copy the directory of the current file relative to its project's
     root directory.
     """
-    def __init__(self, *args, **kwargs):
-        super(CopyFileDirectoryRelativeToProjectCommand, self).__init__(
-            *args, **kwargs)
-        self.relative_path = RelativePath()
-
     def run(self, edit):
-        sublime.set_clipboard(os.path.dirname(self.relative_path(self.view)))
+        (relative_path, _) = self.relative_path(self.view)
+
+        sublime.set_clipboard(os.path.dirname(relative_path))
         sublime.status_message('Copied relative directory')
 
-    def is_enabled(self):
-        return bool(self.relative_path(self.view))
-
-class CopyFilePathAsIncludeMacroCommand(sublime_plugin.TextCommand):
+class CopyFilePathAsIncludeMacroCommand(CFamilyCommand):
     """
     Command to copy the path of the current file relative to its project's root
     directory as a C/C++ #include macro.
     """
-    def __init__(self, *args, **kwargs):
-        super(CopyFilePathAsIncludeMacroCommand, self).__init__(
-            *args, **kwargs)
-        self.relative_path = RelativePath()
-
     def run(self, edit):
-        relative_path = self.relative_path(self.view)
-        relative_path = relative_path.replace(os.path.sep, '/')
-
-        include = '#include "%s"' % (relative_path)
+        header_file = self.to_header_file().replace(os.path.sep, '/')
+        include = '#include "%s"' % (header_file)
 
         sublime.set_clipboard(include)
         sublime.status_message('Copied include guard')
 
-    def is_enabled(self):
-        return bool(self.relative_path(self.view))
-
-class CopyFilePathAsHeaderGuardCommand(sublime_plugin.TextCommand):
+class CopyFilePathAsHeaderGuardCommand(CFamilyCommand):
     """
     Command to copy the path of the current file relative to its project's root
     directory as a C/C++ header guard.
     """
-    def __init__(self, *args, **kwargs):
-        super(CopyFilePathAsHeaderGuardCommand, self).__init__(
-            *args, **kwargs)
-        self.relative_path = RelativePath()
-
     def run(self, edit):
-        relative_path = self.relative_path(self.view)
+        header_file = self.to_header_file().upper() + '_'
+        header_file = re.sub('[^0-9A-Z]+', '_', header_file)
 
-        relative_path = relative_path.upper() + '_'
-        relative_path = re.sub('[^0-9A-Z]+', '_', relative_path)
-
-        sublime.set_clipboard(relative_path)
+        sublime.set_clipboard(header_file)
         sublime.status_message('Copied include guard')
-
-    def is_enabled(self):
-        return bool(self.relative_path(self.view))
